73c9e346db9547fa9e4e0d42b1b62dd28f074644
arfathusain <arfathusain5162@gmail.com>
2022-12-30 21:25:22 +0600

    first commit

---
+++ src/Firebase/Firebase.int.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZQjag-chMCYF2kSNqo-0D5mlYm791zG8",
+  authDomain: "react-login-projects-4.firebaseapp.com",
+  projectId: "react-login-projects-4",
+  storageBucket: "react-login-projects-4.appspot.com",
+  messagingSenderId: "240795433113",
+  appId: "1:240795433113:web:15232d096524a40fc0a008"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
